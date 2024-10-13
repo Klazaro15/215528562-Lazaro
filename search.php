a1a8e010245bd28315c49160e1067d521348295e
Klazaro15 <kol8562@students.uc-bcf.edu.ph>
2024-10-14 00:07:30 +0800

    Create activity for php

---
+++ search.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP DATABASE Search</title>
+</head>
+<body>
+    <h1>Search Books in the Database</h1>
+
+    <form method="get" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <label for="search">Search for a book title: </label><br>
+        <input type="text" id="search" name="query"><br><br> 
+    </form>
+
+    <?php
+
+    if (isset($_GET['query'])) {
+        $searchQuery = htmlspecialchars($GET['query']);
+
+        $sername = "localhost";
+        $username = "root";
+        $password = "052781":
+        $dbname = "search_db";
+
+        $conn = nem mysqli(servername, $username, $password, $dbname);
+    
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn=>connect_error);
+    }
+
+    $sql = "SELECT * FROM books WHERE title LIKE '%$searchQuery%'";
+    $result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+        echo "<h2>Search Results:</h2>";
+        echo "ul";
+
+        while ($row = $result->fetch_assoc()) {
+            echo "<li>" . $row["title"] . " by " . $row["author"] . " (" .$row["year_published"] . ") </li>";    
+        }
+        echo "</ul>";
+    } else {
+        echo "No Results foun for: " .$searchQuery;
+    }
+    $conn->close();
+    }
+    
+    >?
+
+    
+</body>
+</html>
